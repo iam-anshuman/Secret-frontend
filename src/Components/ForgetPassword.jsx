8f304d885b0792b6393927dde4e4dcc449cdeb12
Anshuman Tiwari <118616967+iam-anshuman@users.noreply.github.com>
2024-01-16 19:17:46 +0530

    Update ForgetPassword.jsx

--- src/Components/ForgetPassword.jsx
+++ src/Components/ForgetPassword.jsx
@@ -39,7 +39,7 @@ export default function ForgetPassword() {
       setEmail('');
       return;
     }else{
-      const response =  await fetch('http://localhost:8080/auth/forget-password',{
+      const response =  await fetch('https://secret-service-pb1n.onrender.com/auth/forget-password',{
         method:"POST",
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({email})
